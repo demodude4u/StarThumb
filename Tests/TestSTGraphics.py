8bf39580d14bc2b7508c4370de0b6d4b2e86082b
Demod <demodude4u@gmail.com>
2023-05-03 02:29:49 -0400

    Starting work on STGraphics

---
+++ Tests/TestSTGraphics.py
@@ -0,0 +1,67 @@
+
+from thumbyButton import buttonA, buttonB, buttonU, buttonD, buttonL, buttonR
+
+from STGraphics import convertBMP, fill, blit, blitRotate, display, update
+
+import perf
+
+# BITMAP: width: 25, height: 12
+bmpShip = [bytearray([0, 0, 0, 108, 100, 150, 6, 156, 12, 154, 150, 100, 248, 240, 96, 240, 240, 12, 108, 248, 96, 96, 0, 0, 0,
+           0, 0, 0, 3, 2, 6, 6, 3, 3, 5, 6, 2, 1, 0, 0, 0, 0, 3, 3, 1, 0, 0, 0, 0, 0]),
+           bytearray([0, 12, 12, 104, 108, 254, 254, 240, 240, 240, 244, 244, 248, 240, 240, 144, 144, 252, 156, 252, 240, 240, 0, 0, 0,
+                      0, 3, 3, 1, 3, 7, 7, 0, 0, 0, 2, 2, 1, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0])]
+bmpShipMask = bytearray([12, 158, 158, 254, 254, 255, 255, 255, 255, 255, 255, 254, 254, 248, 248, 252, 252, 254, 254, 254, 252, 248, 248, 248, 240,
+                         3, 7, 7, 7, 7, 15, 15, 15, 15, 15, 15, 7, 7, 1, 1, 3, 3, 7, 7, 7, 3, 1, 1, 1, 0])
+
+buffer = bytearray(72*40)
+
+impShip = convertBMP(25, 12, bmpShip, bmpShipMask)
+
+angle = 0
+pivotX = 12
+pivotFwd = True
+while True:
+    angle = (angle + 5) % 360
+    # if buttonU.justPressed():
+    #     angle = (angle + 5) % 360
+    # if buttonD.justPressed():
+    #     angle = (angle + 360 - 5) % 360
+    # print(angle)
+
+    if pivotFwd:
+        pivotX += 1
+        if pivotX >= 24:
+            pivotFwd = False
+    else:
+        pivotX -= 1
+        if pivotX <= 0:
+            pivotFwd = True
+    # if buttonL.justPressed():
+    #     pivotX -= 1
+    # if buttonR.justPressed():
+    #     pivotX += 1
+    # print(pivotX)
+
+    # perf.start()
+    # impShip = convertBMP(25, 12, bmpShip, bmpShipMask)
+    # perf.stop()
+
+    perf.start()
+    fill(buffer, 0b10)
+    perf.stop()
+
+    perf.start()
+    blit(buffer, impShip, 18-(25 >> 1), 20-(12 >> 1), 25, 12)
+    perf.stop()
+
+    perf.start()
+    blitRotate(buffer, impShip, angle, 42, 14, 25, 12, pivotX, 6)
+    perf.stop()
+
+    perf.start()
+    display(buffer)
+    perf.stop()
+
+    perf.render()
+
+    update()
--- perf.py
+++ perf.py
@@ -1,4 +1,5 @@
 from utime import ticks_us, ticks_diff
+from array import array
 
 from thumbyGrayscale import display
 
@@ -6,39 +7,45 @@ from thumbyGrayscale import display
 bmpDigits = bytearray([31, 17, 31, 0, 31, 0, 29, 21, 23, 21, 21, 31,
                       7, 4, 31, 23, 21, 29, 31, 21, 29, 1, 1, 31, 31, 21, 31, 23, 21, 31])
 
-_startTime = 0
-_endTime = 0
+_start = 0
+_times = array("L", [0, 0, 0, 0, 0])
+_count = 0
 
 
 @micropython.native
 def start():
-    global _startTime
-    _startTime = ticks_us()
+    global _start
+    _start = ticks_us()
 
 
 @micropython.native
-def stop(render=True):
-    global _endTime
-    _endTime = ticks_us()
-    if render:
-        render()
+def stop():
+    end = ticks_us()
+    global _start, _times, _count
+    if _count < 5:
+        _times[_count] = ticks_diff(end, _start)
+        _count += 1
 
 
 @micropython.viper
 def render():
-    global _startTime, _endTime
+    global _times, _count
     bufBW = ptr8(display.buffer)
     bufGS = ptr8(display.shading)
     bd = ptr8(bmpDigits)
-    text = str(ticks_diff(_endTime, _startTime))
-    o = 0
-    for c in text:
-        d = (int(ord(c)) - 48) * 3
-        for _ in range(3):
-            bufBW[o] = (bufBW[o] & 0b11000000) | bd[d]
+    so = 0
+    for i in range(int(_count)):
+        text = str(_times[i])
+        o = so
+        for c in text:
+            d = (int(ord(c)) - 48) * 3
+            for _ in range(3):
+                bufBW[o] = (bufBW[o] & 0b11000000) | bd[d]
+                bufGS[o] &= 0b11000000
+                o += 1
+                d += 1
+            bufBW[o] &= 0b11000000
             bufGS[o] &= 0b11000000
             o += 1
-            d += 1
-        bufBW[o] &= 0b11000000
-        bufGS[o] &= 0b11000000
-        o += 1
+        so += 72
+    _count = 0
